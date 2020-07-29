4ac87edde8b776987e7f18c08a3543944ea86576
YJYandHCX <51406864+YJYandHCX@users.noreply.github.com>
2020-07-29 12:58:35 +0800

    Update localstock.go

--- localstock/localstock.go
+++ localstock/localstock.go
@@ -1,61 +1,22 @@
-//package remotestock
-package remotestock
+package localstock
 
 import (
-	"github.com/garyburd/redigo/redis"
+	"fmt"
 )
 
-type RemoteStock struct {
-	Stock_Num string
-	Total_Num string
-	Order_Num string
+type Localstock struct {
+	//本地的库存
+	LocalInstock int64
+	//销售额
+	LocalSalesVolume int64
 }
 
-//这里是给新的连接池
-func NewPool() *redis.Pool {
-	return &redis.Pool{
-		MaxIdle:   10000,
-		MaxActive: 12000,
-		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "127.0.0.1:6379")
-		},
-	}
+//减少库存的操作
+func (stock *Localstock) LocalDeductionStock() bool {
+	stock.LocalSalesVolume = stock.LocalSalesVolume + 1
+	return stock.LocalSalesVolume <= stock.LocalInstock
 }
 
-//var (
-//	pool *redis.Pool
-//	rc   redis.Conn
-//)
-
-//这里是远程减少redis中间的内存
-func RemoteDeductionStock(conn redis.Conn) bool {
-	var rest int
-	for {
-		b, _ := redis.Int(conn.Do("setnx", "ttt", "sss"))
-		if b == 1 {
-			break
-		}
-	}
-
-	rest, _ = redis.Int(conn.Do("DECR", "stock"))
-	//fmt.Println(rest)
-	conn.Do("DEL", "ttt")
-	if rest >= 0 {
-		return true
-	}
-	return false
-
+func Ttt() {
+	fmt.Println("I love HCX")
 }
-
-//func main() {
-//	pool = NewPool()
-//	rc = pool.Get()0
-//	var i int = 1
-//	for {
-//		if i >= 1 {
-//			break
-//		}
-//	}
-//	RemoteDeductionStock(rc)
-//	fmt.Println("wfewefwefew")
-//}
